eccde46f45b72be48b9ddb5f7b69f381042b368e
nireesha <narendulanireesha@gmail.com>
2026-05-18 23:49:00 -0500

    changed console statement to create merge conflict

--- src/index.js
+++ src/index.js
@@ -6,4 +6,4 @@ for (let i = 0; i < 10; i++) {
 
 console.log("adding console statement to commit");
 
-console.log("New line adding from feature-branch1");
+console.log("New line adding from feature-branch2");
